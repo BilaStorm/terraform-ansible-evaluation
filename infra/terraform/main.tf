b8a01df9af1f959108e50fd3e5acd3322f5c0171
BilaStorm <clementbarret79@gmail.com>
2026-02-02 10:57:55 +0100

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,40 +1,25 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "docker" {}
-
 resource "docker_network" "private_network" {
   name = "tp_network"
 }
 
 resource "docker_container" "app" {
-  name  = "tp-app"
-  image = "tp-app:latest" # Image locale que tu as buildée
-
+  name    = "tp-app"
+  image   = "tp-app:latest"
+  restart = "on-failure"
+  
   networks_advanced {
     name = docker_network.private_network.name
   }
-
-  restart = "on-failure"
 }
-
 
 resource "docker_image" "nginx" {
   name         = "nginx:alpine"
   keep_locally = true
 }
 
-
 resource "docker_container" "nginx" {
   name  = "tp-proxy"
   image = docker_image.nginx.name
-
 
   ports {
     internal = 80
@@ -45,7 +30,6 @@ resource "docker_container" "nginx" {
     name = docker_network.private_network.name
   }
 
-
   volumes {
     host_path      = abspath("${path.module}/nginx.conf")
     container_path = "/etc/nginx/conf.d/default.conf"
@@ -53,7 +37,6 @@ resource "docker_container" "nginx" {
 
   depends_on = [docker_container.app]
 }
-
 
 output "app_url" {
   value = "http://localhost:8080"
